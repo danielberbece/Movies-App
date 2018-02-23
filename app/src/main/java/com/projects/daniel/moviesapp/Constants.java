916ad9b2039fae11b97c1e65dbb487d569decfdc
Daniel Berbece <daniel.berbece97@gmail.com>
2018-02-23 14:08:41 +0200

    API KEY

--- app/src/main/java/com/projects/daniel/moviesapp/Constants.java
+++ app/src/main/java/com/projects/daniel/moviesapp/Constants.java
@@ -4,5 +4,5 @@ package com.projects.daniel.moviesapp;
 public class Constants {
 
     // TMDB API Key must be put here:
-    public static final String API_KEY = "f6c8fb8ef0ae7cb08961df1327b0fa15";
+    public static final String API_KEY = "";
 }
